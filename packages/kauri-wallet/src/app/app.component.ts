7e78f83d5c7efd1d2ad2d976dc36f49932080dc0
Ro Savage <rowan.savage@gmail.com>
2018-06-06 16:32:56 +0200

    Change folder structue, add lerna, and update travis

--- packages/kauri-wallet/src/app/app.component.ts
+++ packages/kauri-wallet/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import lernaImportTest from 'temp-test-module';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Kauri Wallet';
+  lernaTest = lernaImportTest;
 }
---
+++ packages/temp-test-module/index.js
@@ -0,0 +1,1 @@
+module.exports = 'This text exported from temp-test-module'
