ec29d8843c7c2ed417c01067e598694df90cfd34
zanuka <git@zanuka.com>
2018-06-06 16:29:59 +1200

    adds publish script and config

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# die on error
+set -e
+
+echo 'Retrieving latest deploy...'
+url=`curl -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" https://api.netlify.com/api/v1/sites/elastic-hawking-f1d0f1.netlify.com/deploys`
+temp=`echo $url | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w -m 1 'id'`
+
+echo "Publishing build ${temp##*|}..."
+curl -X POST -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" -d "{}" "https://api.netlify.com/api/v1/sites/elastic-hawking-f1d0f1.netlify.com/deploys/${temp##*|}/restore"
