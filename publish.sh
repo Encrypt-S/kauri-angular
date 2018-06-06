385b0390ced4c022f1e256f0df2b2b199c2c4b5c
zanuka <git@zanuka.com>
2018-06-06 17:37:04 +1200

    new script attempt

--- publish.sh
+++ publish.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-# die on error
-set -e
+set -e # halt script on error
 
-echo 'Retrieving latest deploy...'
-url=`curl -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" https://api.netlify.com/api/v1/sites/agathis-australis.netlify.com/deploys`
-temp=`echo $url | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w -m 1 'id'`
+zip -r site.zip dist
 
-echo "Publishing build ${temp##*|}..."
-curl -X POST -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" -d "{}" "https://api.netlify.com/api/v1/sites/agathis-australis.netlify.com/deploys/${temp##*|}/restore"
+curl -H "Content-Type: application/zip" \
+     -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" \
+     --data-binary "@site.zip" \
+     https://api.netlify.com/api/v1/sites/$NETLIFY_SITE_ID/deploys
