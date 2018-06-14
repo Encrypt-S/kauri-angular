12ebf2d61e4b67ae318f3d86ddcf5f03c51db226
Ro Savage <rowan.savage@gmail.com>
2018-06-14 12:55:54 +0200

    Add basic routing

--- packages/kauri-wallet/src/app/app.module.ts
+++ packages/kauri-wallet/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TranslationSampleComponent } from './translation-sample/translation-sample.component';
@@ -9,6 +10,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { SwUpdatePromptComponent } from './features/service-workers/sw-update-prompt/sw-update-prompt.component';
 import { LanguagePickerComponent } from './features/translation/language-picker/language-picker.component';
+import { TestPageComponent } from './pages/test-page/test-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'testpage', component: TestPageComponent },
+];
 
 @NgModule({
   declarations: [
@@ -16,8 +24,14 @@ import { LanguagePickerComponent } from './features/translation/language-picker/
     TranslationSampleComponent,
     SwUpdatePromptComponent,
     LanguagePickerComponent,
+    TestPageComponent,
+    HomePageComponent,
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     FormsModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
