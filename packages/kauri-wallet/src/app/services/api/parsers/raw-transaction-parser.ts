ba58b35be2b4d122f255e4f415fcbf2d37a39e48
zanuka <git@zanuka.com>
2018-06-25 17:04:32 +1200

    fixes lint error and karma conf

--- packages/kauri-wallet/src/app/services/api/parsers/raw-transaction-parser.ts
+++ packages/kauri-wallet/src/app/services/api/parsers/raw-transaction-parser.ts
@@ -3,16 +3,6 @@ import { TxModel } from '../txModel';
 
 export function parseRawTransaction(rawData: any): TxModel[]  {
 
-
-  let data = rawData.data;
-
-  // testing...
-  // data.forEach((item) => {
-  //
-  //   console.log(item);
-  //
-  // });
-
-  return data;
+  return rawData.data;
 
 }
