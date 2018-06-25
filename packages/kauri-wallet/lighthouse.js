e08e8f9586582d2bfcdecc4526ca6d79f9fb9fab
Ro Savage <rowan.savage@gmail.com>
2018-06-25 17:34:02 +0200

    Add lighthouse to travis build

--- packages/kauri-wallet/lighthouse.js
+++ packages/kauri-wallet/lighthouse.js
@@ -14,22 +14,24 @@ const distDir = path.resolve(__dirname, 'dist/kauri-wallet')
 app.use(compression())
 app.use(express.static(distDir))
 
-
-const server = app.listen(3388, () => console.log('Running prod server on http://localhost:3388'))
-
 const opts = {
-  chromeFlags: ['--headless'],
+  chromeFlags: ['--disable-gpu', '--headless', '--no-sandbox'],
 };
+
+function failed(err) {
+  console.log(err)
+  process.exit(1)
+}
 
 function launchChromeAndRunLighthouse(url, opts, config = null) {
   return chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then(chrome => {
     opts.port = chrome.port;
-    console.log('running lighthouse on', url, opts)
+    console.log('running lighthouse on', url)
     return lighthouse(url, opts, config).then(results => {
       delete results.artifacts;
       return chrome.kill().then(() => results)
-    }).catch(err => console.log);
-  }).catch(err => console.log);
+    }).catch(failed);
+  }).catch(failed);
 }
 
 const lighthouseConfig = {
@@ -45,33 +47,36 @@ const lighthouseConfig = {
   },
 }
 
-launchChromeAndRunLighthouse('http://localhost:3388', opts, lighthouseConfig).then(results => {
-  const finalScores = {
-    averageScore: results.reportCategories.map(item => item.score).reduce((acc, curr) => acc + curr, 0) / results.reportCategories.length,
-    catScores: results.reportCategories.map(item => ({ id: item.id, score: item.score })),
-    failed: results.reportCategories.map(item => item.audits) // Get all audit items
-      .reduce((acc, val) => acc.concat(val), []) // flatten 2d array into a single array
-      .filter(item => item.score < 100 && item.result.manual !== true && item.result.weight !== 0) // remove items that passed, and items that a manual checks
-  }
+const server = app.listen(3388, () => {
+  console.log('Running prod server on http://localhost:3388')
+  launchChromeAndRunLighthouse('http://localhost:3388', opts, lighthouseConfig).then(results => {
+    const finalScores = {
+      averageScore: results.reportCategories.map(item => item.score).reduce((acc, curr) => acc + curr, 0) / results.reportCategories.length,
+      catScores: results.reportCategories.map(item => ({ id: item.id, score: item.score })),
+      failed: results.reportCategories.map(item => item.audits) // Get all audit items
+        .reduce((acc, val) => acc.concat(val), []) // flatten 2d array into a single array
+        .filter(item => item.score < 100 && item.result.manual !== true && item.result.weight !== 0) // remove items that passed, and items that a manual checks
+    }
 
-  fs.writeFile(path.resolve(__dirname, 'lighthouse-results.json'), JSON.stringify(finalScores, null, 2), 'utf8', () => { });
+    fs.writeFile(path.resolve(__dirname, 'lighthouse-results.json'), JSON.stringify(finalScores, null, 2), 'utf8', () => { });
 
-  if (finalScores.averageScore < 98) {
-    console.log(finalScores.failed)
-    console.log(`Lighthouse average score was: ${finalScores.averageScore}`)
-    console.log('Score is lower than 99. Fix issues to score 99.')
-    console.log(finalScores.catScores)
+    if (finalScores.averageScore < 98) {
+      console.log(finalScores.failed)
+      console.log(`Lighthouse average score was: ${finalScores.averageScore}`)
+      console.log('Score is lower than 99. Fix issues to score 99.')
+      console.log(finalScores.catScores)
+      server.close()
+      return process.exit(1)
+    }
+
+    console.log('\n', '\x1b[33m', `Lighthouse average score was: ${finalScores.averageScore}`, '\x1b[0m', '\n')
+
+    if (finalScores.averageScore !== 100) {
+      console.log('Some audits did not reach 100%. See lighthouse-results.json for details')
+    }
+
     server.close()
-    return 1
-  }
 
-  console.log('\n', '\x1b[33m', `Lighthouse average score was: ${finalScores.averageScore}`, '\x1b[0m', '\n')
-
-  if (finalScores.averageScore !== 100) {
-    console.log('Some audits did not reach 100%. See lighthouse-results.json for details')
-  }
-
-  server.close()
-
-  return 0
-}).catch(err => console.log);
+    return process.exit(0)
+  }).catch(failed);
+})
