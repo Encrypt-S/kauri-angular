23bb3fbe8e220de5cbc38763a1c210b6c7608361
Ro Savage <rowan.savage@gmail.com>
2018-06-29 21:25:01 +0200

    Fix bug in updateAngularCli.js

--- packages/kauri-wallet/updateAngularCli.js
+++ packages/kauri-wallet/updateAngularCli.js
@@ -13,7 +13,7 @@ fs.readFile(f, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  var result = data.replace(/node: false/g, 'node: node: { fs: \'empty\', global: true,  crypto: true, tls: \'empty\', net: \'empty\', process: true, module: false, clearImmediate: false, setImmediate: false }');
+  var result = data.replace(/node: false/g, 'node: { fs: \'empty\', global: true,  crypto: true, tls: \'empty\', net: \'empty\', process: true, module: false, clearImmediate: false, setImmediate: false }');
 
   fs.writeFile(f, result, 'utf8', function (err) {
     if (err) return console.log(err);
